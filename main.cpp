76881150cc5e245cd5308e61d7eed7c82b1ca9cd
Nomad <nomadSar@yandex.ru>
2025-07-27 18:38:03 +0300

    get_text_document

--- main.cpp
+++ main.cpp
@@ -11,6 +11,18 @@
 using namespace std;
 using json = nlohmann::json;
 
+
+struct Entry {
+    size_t doc_id, count;
+
+// Данный оператор необходим для проведения тестовых сценариев
+    bool operator==(const Entry &other) const {
+        return (doc_id == other.doc_id &&
+                count == other.count);
+    }
+};
+
+
 class ConverterJSON {
 
 
@@ -18,7 +30,7 @@ private:
     string name_program;
     double number_of_verson;
     int max_responses;
-    vector<string> text_file;
+    map<string, string> text_file;
     vector<string> path_file;
     vector<string> requests;
     vector<vector<string>> requestWords;
@@ -57,6 +69,7 @@ public:
             cerr << "config file is missing" << endl;
         }
     }
+
 // метод открывает файл с запросами и сохраняет их.
     void GetRequests() {
         ifstream file_requests_read("requests.json");
@@ -83,20 +96,93 @@ public:
 * @return Возвращает список с содержимым файлов перечисленных
 * в config.json
 */
-//    void GetTextDocuments() {
+    map<string, string> GetTextDocuments() {
+        std::stringstream buffer;
+        for (const auto &path_file: path_file) {
+            ifstream file_read(path_file);
+            if (file_read.is_open()) {
+                if (!text_file.count(path_file)) {
+                    buffer << file_read.rdbuf();
+                    text_file[path_file] = removeLineBreaks(buffer.str());
+                }
+                file_read.close();
+            }
+        }
+        return text_file;
+    };
+
+
+//Функция для замены символов переноса сторики
+    static string removeLineBreaks(const string &input_string) {
+        string result = input_string; // Create a copy of the input string
+
+        // Remove carriage returns ('\r') and newlines ('\n')
+        replace(result.begin(), result.end(), '\r', ' ');
+        replace(result.begin(), result.end(), '\n', ' ');
+
+        return result;
+    };
+};
+
+
+// метод нахождения соответсвий.
+//    void find_request() {
+//
+//
+//        //  Предобработка requests: переводим в нижний регистр и разбиваем на слова
+//
+//        for (string &request: requests) {
+//            request = toLower(request);
+//            requestWords.push_back(splitString(request));
+//        }
+//
+//        string line;
+//        int count;
+//        int _ = 5;
+//        map<int, int> mymap;
+//
 //        for (const auto &n: path_file) {
+//
+//            count = 0;
+//
+//            std::stringstream buffer;
+//            text_file.clear();
 //            ifstream file_read(n);
 //            if (file_read.is_open()) {
-//                string line;
-//                while (getline(file_read, line)) {
-//                    text_file.push_back(line);
-//                }
+//                buffer << file_read.rdbuf();
 //                file_read.close();
+//                string lowerText = toLower(removeLineBreaks(buffer.str()));
+//                searchWords = splitString(lowerText);
+//            };
+//
+//
+//            mymap.clear();
+//
+//
+//            for (int j{0}; j < requests.size(); j++) {
+//                count = 0;
+//
+//                for (const string &searchWord: searchWords) {
+//                    for (size_t x = 0; x < requestWords[j].size(); ++x) {
+//                        if (requestWords[j][x] == searchWord) {
+//                            count++;
+//                        }
+//                    }
+//                }
+//                mymap[j + 1] = +count;
 //            }
+//            for (const auto &[product, price]: mymap)
+//                std::cout << product << "\t" << price << std::endl;
+//            cout << "\n";
 //        }
-//    };
+//    }
+//};
 
-// Функция для преобразования строки в нижний регистр
+class InvertedIndex {
+public:
+    InvertedIndex() = default;
+
+    // Функция для преобразования строки в нижний регистр
     static std::string toLower(const std::string &str) {
 
         std::string result = str; // Создаем копию строки, чтобы не изменять исходную
@@ -116,121 +202,53 @@ public:
         }
         return words;
     }
-//Функция для замены символов переноса сторики
-    static string removeLineBreaks(const string& input_string) {
-        string result = input_string; // Create a copy of the input string
 
-        // Remove carriage returns ('\r') and newlines ('\n')
-        replace(result.begin(), result.end(), '\r', ' ');
-        replace(result.begin(), result.end(), '\n', ' ');
-
-        return result;
-    }
-
-// метод нахождения соответсвий.
-    void find_request() {
-
-
-        //  Предобработка requests: переводим в нижний регистр и разбиваем на слова
-
-        for (string &request: requests) {
-            request = toLower(request);
-            requestWords.push_back(splitString(request));
-        }
-
-        string line;
-        int count;
-        int _ = 5;
-        map<int, int> mymap;
-
-        for (const auto &n: path_file) {
-
-            count = 0;
-
-            std::stringstream buffer;
-            text_file.clear();
-            ifstream file_read(n);
-            if (file_read.is_open()) {
-                buffer << file_read.rdbuf();
-                file_read.close();
-                string lowerText = toLower(removeLineBreaks(buffer.str()));
-                searchWords = splitString(lowerText);
-            };
-
-
-            mymap.clear();
-
-
-            for (int j{0}; j < requests.size(); j++) {
-                count = 0;
-
-                for (const string &searchWord: searchWords) {
-                    for (size_t x = 0; x < requestWords[j].size(); ++x) {
-                        if (requestWords[j][x] == searchWord) {
-                            count++;
-                        }
-                    }
-                }
-                mymap[j + 1] = +count;
+    void UpdateDocumentBase(map<string, string> input_docs) {
+        for (const auto &[name_path, text]: input_docs) {
+            if (!freq_dictionary.count(name_path)) {
+                lowerText = toLower(text);
+                docs = splitString(lowerText);
+                
             }
-            for (const auto &[product, price]: mymap)
-                std::cout << product << "\t" << price << std::endl;
-            cout << "\n";
         }
-    }
+
+    };
+/**
+* Обновить или заполнить базу документов, по которой будем совершать
+поиск
+* @param texts_input содержимое документов
+*/
+
+/**
+* Метод определяет количество вхождений слова word в загруженной базе
+документов
+* @param word слово, частоту вхождений которого необходимо определить
+* @return возвращает подготовленный список с частотой слов
+*/
+    std::vector<Entry> GetWordCount(const std::string& word);
+private:
+    string lowerText;
+    std::vector<std::string> docs; // список содержимого документов
+    std::map<std::string, std::vector<Entry>> freq_dictionary; // частотный
+//    словарь
 };
 
 
-int main() {
+    int main() {
 
-    SetConsoleOutputCP(CP_UTF8);
+        SetConsoleOutputCP(CP_UTF8);
 
-    ConverterJSON file_request;
-    file_request.get_path();
-    file_request.GetRequests();
-//    file_request.GetTextDocuments();
-    file_request.find_request();
+        ConverterJSON file_request;
+        file_request.get_path();
+        file_request.GetRequests();
+        map<string, string> fail = file_request.GetTextDocuments();
+//    file_request.find_request();
+
+        for (const auto& element: fail) {
+            cout << element.first << "\n" << element.second << "\n";
+        }
 
 
-}
+    }
 
-//////  Цикл по text_file
-//for (int i{0}; i < text_file.size(); i++) {
-//string lowerText = toLower(text_file[i]);
-//vector<string> searchWords = splitString(lowerText);
-//int number = max_responses;
-//// 3. Цикл по requests
-//for (int j{0}; j < requests.size(); j++) {
-//int count = 0;
-//
-////  Поиск вхождений
-//for (const string &searchWord: searchWords) {
-//for (size_t x = 0; x < requestWords[j].size(); ++x) {
-//if (requestWords[j][x] == searchWord) {
-//count++;
-//}
-//}
-//}
 
-////  Вычисление rank (с проверкой деления на ноль и более понятной логикой)
-//
-//double rank = 0.0; // Default value
-//if (count == 0) { // Avoid division by zero
-//answers_dict["answers"]["request00" + to_string(j + 1)]["result"] = false;
-//} else {
-//rank = static_cast<double>(count) / static_cast<double>(requestWords[j].size());
-//answers_dict["answers"]["request00" + to_string(j + 1)]["relevance"][i]["docid"] = i + 1;
-//answers_dict["answers"]["request00" + to_string(j + 1)]["relevance"][i]["rank"] = rank;
-//}
-//
-//
-//
-//}
-//}
-//std::cout << std::setw(4) << answers_dict << std::endl;
-//// запись в файл.
-//std::ofstream outputFile("answers.json");
-//outputFile << std::setw(4) << answers_dict << std::endl;
-//
-//
-//outputFile.close();
